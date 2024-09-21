95ecef46e2730ff5f045f4140b36fe059c5acad1
unknown <amiralinayerzadeh@gmail.com>
2024-09-21 19:46:33 +0330

    fix some ui bugs

--- resources/views/home/home/index.blade.php
+++ resources/views/home/home/index.blade.php
@@ -361,15 +361,11 @@
                         slidesPerView: 2,
                         spaceBetween: 20,
                     },
-                    768: {
-                        slidesPerView: 2,
-                        spaceBetween: 40,
-                    },
                     900: {
                         slidesPerView: 3,
                         spaceBetween: 10,
                     },
-                    1080: {
+                    1200: {
                         slidesPerView: 4,
                         spaceBetween: 10,
                     },
